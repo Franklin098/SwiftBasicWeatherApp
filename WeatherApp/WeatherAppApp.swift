5e5ee57e39fadd3ec5505b24f56ffc87f48eb9bd
Franklin Velasquez <iefranklin@hotmail.es>
2023-06-09 22:45:41 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Franklin Velásquez on 9/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
